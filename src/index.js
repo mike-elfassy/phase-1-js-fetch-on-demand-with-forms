ab5b12c04f8fd589194dbf0e31a6b66ac81a2312
Michael Elfassy <mike.elfassy@gmail.com>
2022-12-11 13:48:57 -0500

    commit tree af576f9a3f4ec9893e0a1290e00c8055db429668

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 const init = () => {
-  
+    // Form Handler
+    const inputForm = document.querySelector('form')
+    inputForm.addEventListener('submit', (event) => {
+        // Handle form
+        event.preventDefault()
+        const input = event.target.querySelector('input#searchByID')
+        console.log(input)
+
+        // Fetch data
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            
+            // Display data
+            const title = document.querySelector('section#movieDetails h4')
+            const summary = document.querySelector('section#movieDetails p')
+
+            title.textContent = data.title
+            summary.textContent = data.summary
+        })
+    })
 }
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener('DOMContentLoaded', init)
